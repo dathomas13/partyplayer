42a97761b005f8897d123f1250608cf56b3f8403
Thomas <thomas.friedl@formulastudent-regensburg.de>
2018-05-23 21:02:15 +0200

    implemented qr-code scanning when "join Group"-Button is pressed

--- app/src/main/java/de/gubo_io/partyplayer/SplashScreen.java
+++ app/src/main/java/de/gubo_io/partyplayer/SplashScreen.java
@@ -1,12 +1,20 @@
 package de.gubo_io.partyplayer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class SplashScreen extends AppCompatActivity {
+    private Button mJoinGroupButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,40 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mJoinGroupButton = findViewById(R.id.btnJoinGroup);
+        textView = findViewById(R.id.resultText);
+        final Activity activity = this;
+        mJoinGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(activity);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                intentIntegrator.setPrompt("");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(intentResult != null){
+            if(intentResult.getContents()==null){
+                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "You joined group xyz", Toast.LENGTH_LONG).show();
+                textView.setText(intentResult.getContents());
+            }
+        }
+        else{
+        super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
